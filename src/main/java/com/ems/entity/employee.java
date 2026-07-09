1a2716f4d5c0f074cd4570d57c136433b2012f69
Harisankar <p.harisankar03@gmail.com>
2026-07-09 20:37:12 +0530

    Create Employee entity

---
+++ src/main/java/com/ems/entity/employee.java
@@ -0,0 +1,35 @@
+package com.ems.entity;
+
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(unique = true,nullable = false)
+    private String email;
+
+    @Column(unique = true)
+    private String phone;
+
+
+    private String designation;
+    private double salary;
+}
